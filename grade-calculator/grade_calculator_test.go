df8e4b5662c844631c0ccad6e22f0430b08b5e82
Saanj Patel <saanjpatel@ufl.edu>
2025-09-27 13:54:55 -0400

    add getPassFailFinalGrade unit tests

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -99,3 +99,37 @@ func TestGetGradeD(t *testing.T) {
 		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
 	}
 }
+
+// test pass
+func TestPass(t *testing.T) {
+	expected_value := "Pass"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 100, Assignment)
+	gradeCalculator.AddGrade("exam 1", 100, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 100, Essay)
+
+	actual_value := gradeCalculator.GetPassFailFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected Pass Fail Grade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+// test fail
+func TestFail(t *testing.T) {
+	expected_value := "Fail"
+
+	gradeCalculator := NewGradeCalculator()
+
+	// change grade values so that the actual calculated value would be a F not A
+	gradeCalculator.AddGrade("open source assignment", 10, Assignment)
+	gradeCalculator.AddGrade("exam 1", 20, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 30, Essay)
+	actual_value := gradeCalculator.GetPassFailFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected Pass Fail Grade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
